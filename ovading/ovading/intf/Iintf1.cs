06d665d1e7a7a797b3aa9c8494fe707f0632b23e
ovaya2 <101753395+username@users.noreply.github.com>
2026-04-16 14:36:34 +0200

    update

---
+++ ovading/ovading/intf/Iintf1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ovading.intf
+{
+  interface Iintf1
+  {
+    int int1 { get; set; }
+    string str1 { get; set; }
+
+  }
+}
---
+++ ovading/ovading/serv/intf1_impl1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ovading.intf;
+
+namespace ovading.serv
+{
+  class intf1_impl1 : Iintf1
+  {
+    public int int1 { get; set; }
+    public string str1 { get; set; }
+
+    public intf1_impl1()
+    {
+      str1 = "bbbba";
+      Console.WriteLine(str1);
+    }
+  }
+}
